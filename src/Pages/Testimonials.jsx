7d0b0a180aef7f55638e45f01eac32a3a6a8cb75
hirensbc <hiren.sbc@gmail.com>
2025-09-18 14:03:58 +0530

    fix: fixed font color in about and image brightness in testimonial page

--- src/Pages/Testimonials.jsx
+++ src/Pages/Testimonials.jsx
@@ -30,16 +30,16 @@ const Testimonials = () => {
         <NavBar textColor="text-white" />
       </div>
       <section
-        className="w-full h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center relative"
+        className="w-full h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center relative brightness-65"
         style={{ backgroundImage: `url(${ClientTestimonialsImg})` }}
       >
         {/* <div className="absolute inset-0 bg-opacity-40"></div> */}
         {/* <div className="relative z-10 text-center">
         </div> */}
-          <h1 className="text-white text-4xl md:text-6xl font-mariposa font-bold mb-4">
-            Client Testimonials
-          </h1>
       </section>
+      <h1 className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-white text-4xl md:text-6xl font-mariposa font-bold mb-4">
+        Client Testimonials
+      </h1>
 
       <RealLife
         title="Client Stories"
