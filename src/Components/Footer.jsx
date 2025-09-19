1c0d4294a329e60c3278c12c613e2efc3839b6c3
reeta733 <reetasiya@gmail.com>
2025-09-19 15:00:17 +0530

    Fix : Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,12 +4,12 @@ import logo from "../assets/Images/logo1.png";
 
 export default function Footer() {
   return (
-    <footer className="bg-black  text-white text-sm ">
+    <footer className="bg-black text-white text-sm">
       <div className="w-full border-b border-gray-700 pt-10 px-6 lg:px-20"></div>
 
       <div className="py-10 px-6 lg:px-20">
-        <div className="flex xs:flex-col sm:flex-col lg:flex-row justify-between items-center justify-items-center lg:items-end gap-6">
-          <div className="text-center lg:text-left">
+        <div className="flex flex-col lg:flex-row justify-between items-center lg:items-end gap-6">
+          <div className="text-center lg:text-left flex-1">
             <h2 className="text-2xl font-semibold text-white mb-2 font-mariposa">
               Ready To Glow?
             </h2>
@@ -18,10 +18,11 @@ export default function Footer() {
               more!
             </p>
           </div>
-          <div className="hidden lg:block border-l border-gray-700 h-25"></div>
+          <div className="hidden lg:block border-l border-gray-700 h-25 justify-center"></div>
 
-          <div className="flex-1 w-full lg:max-w-md ">
-            <div className="flex bg-white rounded-full shadow-sm overflow-hidden sm:px-1 xs:px-1">
+          {/* This container's responsiveness has been adjusted */}
+          <div className="flex-1 w-full lg:max-w-md">
+            <div className="flex flex-row items-center bg-white rounded-full shadow-sm overflow-hidden px-1">
               <input
                 type="email"
                 placeholder="Enter email"
@@ -29,7 +30,7 @@ export default function Footer() {
               />
               <button
                 type="submit"
-                className="px-6 py-3 bg-[#BE9B81] text-white text-sm font-medium font-montserrat m-2 rounded-full transition-all duration-300 hover:bg-[#a67f67] hover:shadow-lg "
+                className="px-4 py-2 md:px-6 md:py-3 bg-[#BE9B81] text-white text-sm font-medium font-montserrat m-2 rounded-full transition-all duration-300 hover:bg-[#a67f67] hover:shadow-lg"
               >
                 Subscribe
               </button>
@@ -38,19 +39,17 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="w-full border-b border-gray-700 py-4 px-6 lg:px-20"></div>
-      <div className="py-14 px-6 lg:px-20">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-12 text-center lg:text-left">
-          
-          <div>
-            <h3 className="mb-10">
-              <img
-                src={logo}
-                alt="Unique Euphoria"
-                className="mx-auto lg:mx-0"
-              />
+      <div className="w-full border-b border-gray-700 py-4 px-6 lg:px-20 "></div>
+
+      <div className="py-10 px-6 lg:px-20">
+        <div className="flex flex-col lg:flex-row justify-start lg:gap-[6%]">
+          <div className="text-center lg:text-left w-full lg:w-[35%]">
+           <h3 className="text-white font-semibold mb-4 font-mariposa ">
+             
+              <img src={logo} alt=""  className=" w-[165.75px] h-[49px]  opacity-100" />
             </h3>
-            <p className="text-white text-base leading-7 font-montserrat mb-12">
+
+            <p className="mb-4 text-white text-sm leading-relaxed font-montserrat">
               At Unique Euphoria, we offer premium-quality hair extensions and
               wigs that enhance your beauty and confidence. Elevate your style
               with our luxurious, natural-looking products designed to bring out
@@ -78,69 +77,63 @@ export default function Footer() {
             </div>
           </div>
 
-          {/* Column 2 - Quick Links */}
-          <div>
-            <h3 className="text-white font-semibold mb-6 font-mariposa">
-              Quick Links
-            </h3>
-            <ul className="space-y-4 font-montserrat text-base">
-              {[
-                "Home",
-                "Products",
-                "About Us",
-                "Awards",
-                "Video Review",
-                "Testimonials",
-                "Blogs",
-              ].map((item) => (
-                <li key={item}>
-                  <a
-                    href="#"
-                    className="hover:text-white transition-colors duration-300"
-                  >
-                    {item}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          {/* Column 3 - Our Services */}
-          <div>
-            <h3 className="text-white font-semibold mb-6 font-mariposa">
-              Our Services
-            </h3>
-            <ul className="space-y-4 font-montserrat text-base">
-              {["Volume", "Length", "Coverage", "Style", "Color"].map(
-                (service) => (
-                  <li key={service}>
+          <div className="flex flex-col sm:flex-row justify-between w-full lg:w-auto mt-10 lg:mt-0">
+            <div className="text-center sm:text-left sm:pr-10 lg:pr-16 lg:ml-20">
+              <h3 className="text-white font-semibold mb-4 font-mariposa">
+                Quick Links
+              </h3>
+              <ul className="space-y-2 font-montserrat">
+                {[
+                  "Home",
+                  "Products",
+                  "About Us",
+                  "Awards",
+                  "Video Review",
+                  "Testimonials",
+                  "Blogs",
+                ].map((item) => (
+                  <li key={item}>
                     <a
                       href="#"
                       className="hover:text-white transition-colors duration-300"
                     >
-                      {service}
+                      {item}
                     </a>
                   </li>
-                )
-              )}
-            </ul>
-          </div>
+                ))}
+              </ul>
+            </div>
 
-          {/* Column 4 - Get in Touch */}
-          <div>
-            <h3 className="text-white font-semibold mb-6 font-mariposa">
-              Get in Touch
-            </h3>
-            <ul className="space-y-4 font-montserrat text-base">
-              <li>+91 00000 00000</li>
-              <li>
-                Budhia Jadawadi,
-                <br />
-                Opp Digital Equipment Ltd,
-                <br />
-                Mumbai
-              </li>
-            </ul>
+            <div className="text-center sm:text-left sm:pr-10 lg:pr-16 lg:ml-20">
+              <h3 className="text-white font-semibold mb-4 font-mariposa">
+                Our Services
+              </h3>
+              <ul className="space-y-2 font-montserrat">
+                {["Volume", "Length", "Coverage", "Style", "Color"].map(
+                  (service) => (
+                    <li key={service}>
+                      <a
+                        href="#"
+                        className="hover:text-white transition-colors duration-300"
+                      >
+                        {service}
+                      </a>
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
+            
+            <div className="text-center sm:text-left sm:pr-10 lg:pr-16 lg:ml-20">
+              <h3 className="text-white font-semibold mb-4 tracking-wide font-mariposa">Get in Touch</h3>
+              <address className="space-y-1 not-italic font-montserrat text-sm text-white">
+                <p>+91 00000 00000</p>
+                <p>Budhia Jadawadi,</p>
+                <p>Opp Digital</p>
+                <p>Equipment Ltd,</p>
+                <p>Mumbai</p>
+              </address>
+            </div>
           </div>
         </div>
       </div>
@@ -151,7 +144,6 @@ export default function Footer() {
         <p className="text-center font-montserrat w-full lg:absolute lg:left-1/2 lg:-translate-x-1/2">
           © 2025 Unique Euphoria. All rights reserved.
         </p>
-
         <div className="flex space-x-6 lg:ml-auto mt-2 lg:mt-0 px-18">
           <a href="#" className="hover:underline font-montserrat">
             Terms and Condition
